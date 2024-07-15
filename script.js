db74f98bcae5868544488258c4825f9e2135cf0a
abdul rafey <abdulrafey12389@gmail.com>
2024-07-15 00:43:19 -0700

    dashboard

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 var swiper = new Swiper('.swiper', {
     autoplay: {
-      delay: 2000, // Delay in milliseconds between each slide
-      disableOnInteraction: false, // Autoplay will not be disabled after user interactions
+      delay: 2000,
+      disableOnInteraction: false, 
     },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-    // Enable debugger
-    // debugger: true,
   });
